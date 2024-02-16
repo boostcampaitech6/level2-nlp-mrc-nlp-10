cc722146fcc40fca9632b583792e39c1ff1eacfc
BJH9 <tfzangin9@gmail.com>
2024-02-16 09:39:35 +0000

    [feat]: transfer learning 구현 #9

--- code/utils_qa.py
+++ code/utils_qa.py
@@ -25,7 +25,7 @@ from typing import Any, Optional, Tuple
 
 import numpy as np
 import torch
-from arguments import DataTrainingArguments, ModelArguments
+from arguments import DataTrainingArguments, ModelArguments, TransferArguments
 from datasets import DatasetDict
 from tqdm.auto import tqdm
 from transformers import PreTrainedTokenizerFast, TrainingArguments, is_torch_available
@@ -354,3 +354,50 @@ def check_no_error(
     if "validation" not in datasets:
         raise ValueError("--do_eval requires a validation dataset")
     return last_checkpoint, max_seq_length
+
+def transfer_check_no_error(
+    data_args: DataTrainingArguments,
+    training_args: TrainingArguments,
+    transfer_args: TransferArguments,
+    datasets: DatasetDict,
+    tokenizer,
+) -> Tuple[Any, int]:
+
+    # last checkpoint 찾기.
+    last_checkpoint = None
+    if (
+        os.path.isdir(training_args.output_dir)
+        and (training_args.do_train or transfer_args.do_transfer)
+        and not training_args.overwrite_output_dir
+    ):
+        last_checkpoint = get_last_checkpoint(training_args.output_dir)
+        if last_checkpoint is None and len(os.listdir(training_args.output_dir)) > 0:
+            raise ValueError(
+                f"Output directory ({training_args.output_dir}) already exists and is not empty. "
+                "Use --overwrite_output_dir to overcome."
+            )
+        elif last_checkpoint is not None:
+            logger.info(
+                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
+                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
+            )
+
+    # Tokenizer check: 해당 script는 Fast tokenizer를 필요로합니다.
+    if not isinstance(tokenizer, PreTrainedTokenizerFast):
+        raise ValueError(
+            "This example script only works for models that have a fast tokenizer. Checkout the big table of models "
+            "at https://huggingface.co/transformers/index.html#bigtable to find the model types that meet this "
+            "requirement"
+        )
+
+    if data_args.max_seq_length > tokenizer.model_max_length:
+        logger.warn(
+            f"The max_seq_length passed ({data_args.max_seq_length}) is larger than the maximum length for the"
+            f"model ({tokenizer.model_max_length}). Using max_seq_length={tokenizer.model_max_length}."
+        )
+    max_seq_length = min(data_args.max_seq_length, tokenizer.model_max_length)
+
+    if "validation" not in datasets:
+        raise ValueError("--do_eval requires a validation dataset")
+    return last_checkpoint, max_seq_length
+
